4485ea7dbe22e197ba831c900b3a2522d0416e8e
323 <323@323-9>
2024-12-24 10:12:05 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public List<Product> products = new List<Product>();
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Название склада: {Name}, Адрес: {Adress},\n Список товаров:");
+            foreach (Product product in products)
+            {
+                product.Show();
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public Product(string name, int num)
+        {
+            Name = name;
+            Number = num;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Продукт: {Name}, Количество: {Number}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ClassLibrary1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Картошка", 200);
+            Product product1 = new Product("Свёкла", 400);
+            Product product2 = new Product("Мороженное", 150);
+            Product product3 = new Product("Соль", 300);
+
+            Store store = new Store();
+            store.Name = "Склад Москвы №1";
+            store.Adress = "Ул. Первая, строение 2";
+
+            store.products.Add(product);
+            store.products.Add(product1);
+            store.products.Add(product2);
+            store.products.Add(product3);
+
+            store.ShowAll();
+            Console.ReadLine();
+        }
+    }
+}
